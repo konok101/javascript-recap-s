038c389500ce6a1c0133d7f16d49d857d927cdc6
konok101 <rockykonok@gmail.com>
2022-09-11 19:54:49 +0600

    ascynous function like as setTimeout, setInterval

---
+++ javascriptCode_38/setTimeOut.js
@@ -0,0 +1,12 @@
+console.log('1');
+ 
+const doSomething =()=>{
+    console.log('SS')
+
+}
+console.log('2');
+console.log('3');
+setTimeout( function(){
+console.log('SASASA');
+}, 00);
+console.log('99')
