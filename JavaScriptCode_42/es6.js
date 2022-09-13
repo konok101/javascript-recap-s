17d7aae41ee41a2c1e85fb99d1d3d5109e9bcbdd
konok101 <rockykonok@gmail.com>
2022-09-13 22:56:01 +0600

    spread operator done

---
+++ JavaScriptCode_42/es6.js
@@ -0,0 +1,17 @@
+//template string
+
+// arroe function
+
+//spread operator
+
+const numbers=[1,2,4,22,44,33];
+
+const newNumbers=[...numbers];
+numbers.push(1);
+numbers.push(1);
+numbers.push(1);
+
+//spread operator
+console.log(newNumbers);
+
+console.log(numbers);
