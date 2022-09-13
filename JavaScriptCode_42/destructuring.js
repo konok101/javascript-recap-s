25a0929b47709e6848d3c58f3feb4a332ac16934
konok101 <rockykonok@gmail.com>
2022-09-14 00:49:17 +0600

    destructing done

---
+++ JavaScriptCode_42/destructuring.js
@@ -0,0 +1,33 @@
+//array destructing
+const number = [21, 22];
+//array distructing
+const [x, y] = [44, 33];
+console.log(x, y);
+
+//function destrucing
+
+function boxify(a, b) {
+    const ac = [a, b];
+    return ac;
+}
+const [e, r] = boxify(30, 40);
+console.log(boxify(e,r));
+
+const employs={
+    id: 'vs code',
+    language: ['ss','js','ee','we'],
+    specifiction: {
+        name:'wwww',
+        watch:{
+            color:'black',
+            code:{
+                lan:33
+            }
+        }
+    }
+
+    
+}
+
+const {language, lan}=employs?.specifiction?.watch?.code;
+console.log( lan);
